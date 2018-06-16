7a8df72a7ff8aaa85af5a35eef363c2e6f4d2f6e
Sanket <san1646@gmail.com>
2018-06-16 18:39:31 -0400

    Update ProjectEuler.java

--- src/test/one/ProjectEuler.java
+++ src/test/one/ProjectEuler.java
@@ -11,8 +11,10 @@ import java.math.BigInteger;
 
 public class ProjectEuler {
     public static void main(String[] args) {
-        System.out.println("largest prime: "+largestPrimeFactor(new Long("600851475143")));
-        //System.out.println(isPrime(new Long("6857")));
+        //System.out.println("largest prime: " + largestPrimeFactor(new Long("600851475143")));
+        // System.out.println(isPrime(new Long("6857")));
+        //System.out.println(isPalindrome("9009"));//"AManAPlanACanalPanama"
+        System.out.println(largestPalindromeProduct());
     }
 
     /**
@@ -35,6 +37,51 @@ public class ProjectEuler {
         return elPrimo;
     }
 
+    /**
+     * https://projecteuler.net/problem=4
+     * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit
+     * numbers is 9009 = 91 × 99.
+     * 
+     * Find the largest palindrome made from the product of two 3-digit numbers.
+     * 913*993
+     * @return
+     */
+    static int largestPalindromeProduct() {
+        int P_999_999 = 999 * 999;
+        int P_9999_9999 = 9999*9999;
+        int P_100_100 = 100 * 100;
+        int P_1000_1000 = 1000*1000;
+        int i = P_999_999;
+        int palindrome = 0;
+        for (; i > P_100_100; i--) {
+            if (isPalindrome("" + i)) {
+                System.out.println("------------------");
+                for (int j = 999; j > 99; j--) {
+                    System.out.println("trying " + i + " for..." + j);
+                    if (i % j == 0 && (String.valueOf(i/j).length()==3) ) {
+                        palindrome = i;
+                        break;
+                    }
+                }
+                if(palindrome!=0) break;
+            }
+        }
+        return palindrome;
+    }
+    
+
+    static boolean isPalindrome(String p) {
+        if(p==null) return false;
+        String s = p.toLowerCase();
+        
+        //if length is even, split and compare
+        for (int i=0, j=s.toCharArray().length-1; i<=j; i++, j--) {
+            if(s.toCharArray()[i]!=s.toCharArray()[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
     static boolean isPrime(Long x) {
         if (x <= 1)
             return false;
